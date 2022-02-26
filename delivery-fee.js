c84075413e10f07380272fe3dfa34d38a7961e38
yingyueh <97005138+yingyueh@users.noreply.github.com>
2022-02-26 11:26:41 +0800

    Update delivery-fee.js

--- delivery-fee.js
+++ delivery-fee.js
@@ -4,20 +4,22 @@ const country = document.querySelector("#country");
 const numberInput = document.querySelector("#delivery-fee");
 const spNum = document.querySelector("#spNum");
 const spInput = document.querySelector("#spInput");
+const calc = document.querySelector(".calc");
 
 mpayInput.value = mpay.innerText; //傳入定期定額的金額到第一個input值
+calcTotal();
 
 //加碼贊助: 當加碼贊助input值改變，一併更新第二個input值
 function sendSpnum() {
   spInput.value = spNum.value;
+  calcTotal();
 }
 
 //運費：當下選單的值改變時，同時也更新第三個input值
 function sendDfee() {
-  console.log(country.value);
   const selectedCountry = country.value;
   if (selectedCountry == "台灣本島") {
-    numberInput.value = "30";
+    numberInput.value = 30;
   } else if (selectedCountry == "台灣外島(需額外運費)") {
     numberInput.value = 60;
   } else if (selectedCountry == "香港") {
@@ -27,4 +29,14 @@ function sendDfee() {
   } else {
     return;
   }
+  calcTotal();
+}
+
+//計算總額
+function calcTotal() {
+  const result = document.querySelector("#result");
+  result.value =
+    parseInt(mpayInput.value) +
+    parseInt(numberInput.value) +
+    parseInt(spInput.value);
 }
